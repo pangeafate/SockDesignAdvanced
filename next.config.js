c5f8e74ae52daa330110c0cbc514ac6fe60f8439
pangeafate <pangeafate@gmail.com>
2025-06-30 15:47:05 +0300

    Fix basePath for SockDesignAdvanced and add deployment workflow

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/SockDesign',
+  basePath: '/SockDesignAdvanced',
   images: {
     unoptimized: true,
     remotePatterns: [
